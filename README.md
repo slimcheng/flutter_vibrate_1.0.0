e65f2caf332e0a0be160f1be5a0519bf92f7e826
slimcheng <cmrabc@gmail.com>
2022-01-08 13:50:00 +0800

    commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Vibrate
+[![pub package](https://img.shields.io/pub/v/flutter_vibrate.svg)](https://pub.dartlang.org/packages/flutter_vibrate)
+
+A Flutter plugin to vibrate the device. 
+This uses all the current Haptic Feedback APIs from Apple and provides similar feedback on Android.
+
+## Usage
+To use this plugin, add `flutter_vibrate` as a [dependency in your pubspec.yaml file](https://flutter.io/platform-plugins/).
+
+Make sure you add the following permissions to your Android Manifest
+``` xml
+<uses-permission android:name="android.permission.VIBRATE"/>
+```
+## Usage
+``` dart
+// Import package
+import 'package:flutter_vibrate/flutter_vibrate.dart';
+```
+
+### Vibration
+``` dart
+// Check if the device can vibrate
+bool canVibrate = await Vibrate.canVibrate;
+
+// Vibrate
+// Vibration duration is a constant 500ms because
+// it cannot be set to a specific duration on iOS.
+Vibrate.vibrate();
+
+// Vibrate with pauses between each vibration
+final Iterable<Duration> pauses = [
+    const Duration(milliseconds: 500),
+    const Duration(milliseconds: 1000),
+    const Duration(milliseconds: 500),
+];
+// vibrate - sleep 0.5s - vibrate - sleep 1s - vibrate - sleep 0.5s - vibrate
+Vibrate.vibrateWithPauses(pauses);
+```
+### Haptic Feedback
+``` dart
+// Choose from any of these available methods
+enum FeedbackType {
+  success,
+  error,
+  warning,
+  selection,
+  impact,
+  heavy,
+  medium,
+  light
+}
+
+var _type = FeedbackType.impact;
+Vibrate.feedback(_type);
+```
+## Documentation
+#### Android
+
+https://developer.android.com/reference/android/view/HapticFeedbackConstants
+
+#### iOS
+
+https://developer.apple.com/design/human-interface-guidelines/ios/user-interaction/feedback/
+
---
+++ example/lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _canVibrate = true;
+  final Iterable<Duration> pauses = [
+    const Duration(milliseconds: 500),
+    const Duration(milliseconds: 1000),
+    const Duration(milliseconds: 500),
+  ];
+
+  @override
+  initState() {
+    super.initState();
+    init();
+  }
+
+  init() async {
+    bool canVibrate = await Vibrate.canVibrate;
+    setState(() {
+      _canVibrate = canVibrate;
+      _canVibrate
+          ? print("This device can vibrate")
+          : print("This device cannot vibrate");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Haptic Feedback Example')),
+        body: Center(
+          child: ListView(children: [
+            ListTile(
+              title: Text("Vibrate"),
+              leading: Icon(Icons.vibration, color: Colors.teal),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.vibrate();
+                    },
+            ),
+            ListTile(
+              title: Text("Vibrate with Pauses"),
+              leading: Icon(Icons.vibration, color: Colors.brown),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.vibrateWithPauses(pauses);
+                    },
+            ),
+            Divider(height: 1.0),
+            ListTile(
+              title: Text("Impact"),
+              leading: Icon(Icons.tap_and_play, color: Colors.orange),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.feedback(FeedbackType.impact);
+                    },
+            ),
+            ListTile(
+              title: Text("Selection"),
+              leading: Icon(Icons.select_all, color: Colors.blue),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.feedback(FeedbackType.selection);
+                    },
+            ),
+            ListTile(
+              title: Text("Success"),
+              leading: Icon(Icons.check, color: Colors.green),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.feedback(FeedbackType.success);
+                    },
+            ),
+            ListTile(
+              title: Text("Warning"),
+              leading: Icon(Icons.warning, color: Colors.red),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.feedback(FeedbackType.warning);
+                    },
+            ),
+            ListTile(
+              title: Text("Error"),
+              leading: Icon(Icons.error, color: Colors.red),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.feedback(FeedbackType.error);
+                    },
+            ),
+            Divider(height: 1.0),
+            ListTile(
+              title: Text("Heavy"),
+              leading: Icon(Icons.notification_important, color: Colors.red),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.feedback(FeedbackType.heavy);
+                    },
+            ),
+            ListTile(
+              title: Text("Medium"),
+              leading: Icon(Icons.notification_important, color: Colors.green),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.feedback(FeedbackType.medium);
+                    },
+            ),
+            ListTile(
+              title: Text("Light"),
+              leading:
+                  Icon(Icons.notification_important, color: Colors.yellow[700]),
+              onTap: !_canVibrate
+                  ? null
+                  : () {
+                      Vibrate.feedback(FeedbackType.light);
+                    },
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
